c6568ebc70adf9fb0fb6c1745737ae6945576813
jothepro <github@jothe.pro>
2022-08-29 20:13:43 +0200

    improve layout for long templated functions
    - template parameters can wrap to the next line when they get too long
    - return types can wrap on small screens when they get too long
    - additional spacing was added to the member declarations table
    - add support for class template parameters
    
    fixes #84

--- include/MyLibrary/subclass-example.hpp
+++ include/MyLibrary/subclass-example.hpp
@@ -8,6 +8,7 @@ namespace MyLibrary {
     /**
      * @brief some subclass
      */
+    template<typename TemplatedClass>
     class SubclassExample : public Example {
     public:
 
@@ -18,13 +19,22 @@ namespace MyLibrary {
         int virtualfunc() override;
 
         /**
-         * @brief Extra long function with lots of parameters
+         * @brief Template function function
+         */
+        template <typename T>
+        std::shared_ptr<std::string> function_template_test(std::shared_ptr<T>& param);
+
+        /**
+         * @brief Extra long function with lots of parameters and many template types.
+         *
+         * Also has a long return type.
+         *
          * @param param1 first parameter
          * @param param2 second parameter
          * @param parameter3 third parameter
          */
-        template <typename T>
-        std::shared_ptr<std::string> long_function_with_many_parameters(std::shared_ptr<T>& param1, std::shared_ptr<std::string>& param2, bool parameter3) {
+        template <typename T, typename Foo, typename Bar, typename Alice, typename Bob, typename Charlie, typename Hello, typename World>
+        std::pair<std::string, std::string> long_function_with_many_parameters(std::shared_ptr<T>& param1, std::shared_ptr<std::string>& param2, bool parameter3, Alice paramater4 Bob parameter 5) {
             if(true) {
                 std::cout << "this even has some code." << std::endl;
             }
