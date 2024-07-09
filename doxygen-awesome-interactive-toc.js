3bf105abd2f9a47e0be9a3ba4ec04553d7a9bc64
Ayush Chamoli <75367725+iamAyushChamoli@users.noreply.github.com>
2024-07-09 10:37:31 +0530

    Update doxygen-awesome-interactive-toc.js
    Updated the throttle function with jothepro's solution.

--- doxygen-awesome-interactive-toc.js
+++ doxygen-awesome-interactive-toc.js
@@ -77,15 +77,15 @@ class DoxygenAwesomeInteractiveToc {
         active?.classList.remove("aboveActive")
     }
 
-    static throttle(func, delay) {
-    let lastCall = 0;
-    return function (...args) {
-        const now = new Date().getTime();
-        if (now - lastCall < delay) {
-            return;
-        }
-        lastCall = now;
-        return func(...args);
-    };
-}
+        static throttle(func, delay) {
+        let lastCall = 0;
+        return function (...args) {
+            const now = new Date().getTime();
+            if (now - lastCall < delay) {
+                return;
+            }
+            lastCall = now;
+            return setTimeout(() => {func(...args)}, delay);
+        };
+    }
 }
